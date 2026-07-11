14a10b26e3a79c2a7dff4d8872e252ddc30c7f20
gritu <gritus22324@gmail.com>
2026-07-11 14:55:38 +0300

    - Внесены изменения в настройки; - Добавлена команда для создания админа; - Добавлены и проведены миграции.

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 
+
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -143,3 +144,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'users.User'

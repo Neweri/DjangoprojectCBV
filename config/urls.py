66842d99d2c2b37f0d58437ce55a4d6c10f1b46b
gritu <gritus22324@gmail.com>
2026-07-12 15:12:00 +0300

    - Обновили urls - Добавлен index view - Добавлен url для index view

--- config/urls.py
+++ config/urls.py
@@ -22,6 +22,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
                 path('admin/', admin.site.urls),
-                # path('', include('dogs.urls', namespace='dogs'))
+                path('', include('dogs.urls', namespace='dogs')),
             ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 

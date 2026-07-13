ba5aaff3e352868a67edb2fd13e53087b0d0efc9
gritu <gritus22324@gmail.com>
2026-07-13 20:45:45 +0300

    - Прописан backend для CRUD модели DOG; - Прописаны нужные url's; - Добавлены пустые шаблоны под каждую view

--- dogs/urls.py
+++ dogs/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from dogs.views import index, breeds_list, breeds_dogs_list, dogs_list_view, dog_create_view
+from dogs.views import index, breeds_list, breeds_dogs_list, dogs_list_view, dog_create_view, dog_detail_view, dog_update_view
 from dogs.apps import DogsConfig
 
 
@@ -14,4 +14,6 @@ urlpatterns = [
     #dogs
     path('dogs/', dogs_list_view, name='dogs_list'),
     path('dogs/create/', dog_create_view, name='dog_create'),
+    path('dogs/detail/<int:pk>/', dog_detail_view, name='dog_detail'),
+    path('dogs/update/<int:pk>/', dog_update_view, name='dog_update'),
 ]

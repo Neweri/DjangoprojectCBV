bba0b5c2674e31689ccef26ac43c3d46aa9f5237
gritu <gritus22324@gmail.com>
2026-07-13 20:57:11 +0300

    - Добавлен шаблонный тег для загрузки изображений карточек.

---
+++ dogs/templatetags/my_tags.py
@@ -0,0 +1,11 @@
+from django import template
+
+
+register = template.Library
+
+
+@register.filter()
+def dogs_media(val):
+    if val:
+        return fr'/media/{val}'
+    return '/static/dummydog.jpg'

05c87a6cd9506d4ffbd5d20180c0235bda3414e7
gritu <gritus22324@gmail.com>
2026-07-17 18:28:05 +0300

    - Написан функционал аутентификации пользователя

--- users/views.py
+++ users/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth import authenticate, login, logout
 
-from users.forms import UserRegisterForm
+from users.forms import UserRegisterForm, UserLoginForm
 
 
 def user_register_view(request):
@@ -18,3 +19,22 @@ def user_register_view(request):
         'form': UserRegisterForm
     }
     return render(request, 'users/register.html', context=context)
+
+
+def user_login_view(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(email=cd['email'], password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponseRedirect(reverse('dogs:index'))
+                else:
+                    return HttpResponse('Аккаунт неактивен!')
+    context = {
+        'title': 'Авторизация',
+        'form': UserLoginForm
+    }
+    return render(request, 'users/login.html', context=context)

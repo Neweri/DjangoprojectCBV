61dca199e71bf5eecd6e99a7c9ad257d8943596e
gritu <gritus22324@gmail.com>
2026-07-17 17:59:29 +0300

    - Написан функционал регистрации пользователя

--- users/admin.py
+++ users/admin.py
@@ -7,6 +7,6 @@ from users.models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'last_name', 'first_name',)
+    list_display = ('pk', 'email', 'last_name', 'first_name',)
     list_filter = ('last_name',)
 

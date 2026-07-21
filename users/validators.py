0ae2fb52df7b3630662aa30f812738eeef0074dc
gritu <gritus22324@gmail.com>
2026-07-21 19:22:39 +0300

    - Добавлен второй язык в валидатор

--- users/validators.py
+++ users/validators.py
@@ -1,12 +1,25 @@
 import re
+
+from django.conf import settings
 from django.core.exceptions import ValidationError
 
 
 def validate_password(field):
     pattern = re.compile(r'^[A-Za-z0-9]')
+    language = settings.LANGUAGE_CODE
+    error_massages = [
+        {
+            'ru-ru': 'Пароль должен содержать только латинские буквы и цифры',
+            'en-us': 'Password must contain A-Z a-z letters and 0-9 numbers'
+        },
+        {
+            'ru-ru': 'Пароль должен содержать от 8 до 16 символов',
+            'en-us': 'Password length must be between 8 and 16 charters'
+        }
+    ]
     if not bool(re.match(pattern, field)):
-        print('Пароль должен содержать только латинские буквы и цифры')
-        raise ValidationError('Пароль должен содержать только латинские буквы и цифры')
+        print(error_massages[0][language])
+        raise ValidationError(error_massages[0][language], code=error_massages[0][language])
     if not 8 <= len(field) <= 16:
-        print('Пароль должен содержать от 8 до 16 символов')
-        raise ValidationError('Пароль должен содержать от 8 до 16 символов')
+        print(error_massages[1][language])
+        raise ValidationError(error_massages[1][language], code=error_massages[1][language])

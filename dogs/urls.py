9a361e00622f5801b41ca443cee45dff451c855e
gritu <gritus22324@gmail.com>
2026-07-28 19:21:57 +0300

    - dog_delete view переписан на CBV - внесены изменения в urls

--- dogs/urls.py
+++ dogs/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from dogs.views import index, breeds_list, breeds_dogs_list, DogListView, DogCreateView, DogDetailView, dog_update_view, dog_delete_view
+from dogs.views import index, breeds_list, breeds_dogs_list, DogListView, DogCreateView, DogDetailView, DogUpdateView, DogDeleteView
 from dogs.apps import DogsConfig
 
 
@@ -15,6 +15,6 @@ urlpatterns = [
     path('dogs/', DogListView.as_view(), name='dogs_list'),
     path('dogs/create/', DogCreateView.as_view(), name='dog_create'),
     path('dogs/detail/<int:pk>/', DogDetailView.as_view(), name='dog_detail'),
-    path('dogs/update/<int:pk>/', dog_update_view, name='dog_update'),
-    path('dogs/delete/<int:pk>/', dog_delete_view, name='dog_delete'),
+    path('dogs/update/<int:pk>/', DogUpdateView.as_view(), name='dog_update'),
+    path('dogs/delete/<int:pk>/', DogDeleteView.as_view(), name='dog_delete'),
 ]

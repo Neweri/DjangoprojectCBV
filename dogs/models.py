09716538ee40b19b62782e057c68296efac71f5a
gritu <gritus22324@gmail.com>
2026-07-28 19:35:18 +0300

    - Добавлена модель для родословной - Проведены миграции

--- dogs/models.py
+++ dogs/models.py
@@ -29,3 +29,17 @@ class Dog(models.Model):
     class Meta:
         verbose_name = 'dog'
         verbose_name_plural = 'dogs'
+
+
+class DogParent(models.Model):
+    dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+    name = models.CharField(max_length=150, verbose_name='Кличка родителя')
+    breed = models.ForeignKey(Breed, on_delete=models.CASCADE, verbose_name='Порода родителя')
+    birth_date = models.DateField(**NULLABLE, verbose_name='Дата рождения родителя')
+
+    def __str__(self):
+        return f'{self.name} ({self.breed})'
+
+    class Meta:
+        verbose_name = 'parent'
+        verbose_name_plural = 'parents'

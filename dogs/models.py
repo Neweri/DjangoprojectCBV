2bacca473fd38d149b5041f6f45b058b67f5879f
gritu <gritus22324@gmail.com>
2026-07-28 20:48:08 +0300

    - Добавлено поле активности к карточке собаки

--- dogs/models.py
+++ dogs/models.py
@@ -36,6 +36,8 @@ class DogParent(models.Model):
     name = models.CharField(max_length=150, verbose_name='Кличка родителя')
     breed = models.ForeignKey(Breed, on_delete=models.CASCADE, verbose_name='Порода родителя')
     birth_date = models.DateField(**NULLABLE, verbose_name='Дата рождения родителя')
+    is_active = models.BooleanField(default=True, verbose_name='Активность')
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, **NULLABLE, verbose_name='Хозяин')
 
     def __str__(self):
         return f'{self.name} ({self.breed})'

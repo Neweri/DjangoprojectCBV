9fb07bf042145dbb47ebe64def6939d854c7f659
gritu <gritus22324@gmail.com>
2026-08-03 19:44:52 +0300

    - Добавлено поле просмотров к карточке, проведены миграции, добавлен метод увелечения просмотров - Обновлен шаблон dogs.html - Обновлена фикстура.

--- dogs/models.py
+++ dogs/models.py
@@ -23,9 +23,14 @@ class Dog(models.Model):
     birth_date = models.DateField(**NULLABLE, verbose_name='Дата рождения')
     is_active = models.BooleanField(default=True, verbose_name='Активность')
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, **NULLABLE, verbose_name='Хозяин')
+    views = models.ImageField(default=0, verbose_name='Просмотры')
 
     def __str__(self):
         return f'{self.name} ({self.breed})'
+
+    def views_count(self):
+        self.views += 1
+        self.save()
 
     class Meta:
         verbose_name = 'dog'

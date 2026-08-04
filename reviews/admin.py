504f0a72602ca2557d9567730fd4079d47b5ee6e
gritu <gritus22324@gmail.com>
2026-08-04 20:55:53 +0300

    - Добавлен ReviewListView - Добавлен ReviewDeactivatedListView - Созданы шаблоны для Reviews - Внесены необходимые изменения в текуще шаблоны

--- reviews/admin.py
+++ reviews/admin.py
@@ -4,7 +4,7 @@ from reviews.models import Review
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
-    list_display = ('title', 'dog', 'author', 'created', 'sing_of_review')
+    list_display = ('title', 'dog', 'author', 'created', 'sign_of_review')
     ordering = ('created', )
     list_filter = ('dog', 'author',)
 

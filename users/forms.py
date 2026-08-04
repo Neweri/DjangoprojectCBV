4c95ec081c1012a5a290e1bedd97ef2d23a8b8a4
gritu <gritus22324@gmail.com>
2026-08-04 18:53:34 +0300

    - Добавлено новое поле в модель пользователя

--- users/forms.py
+++ users/forms.py
@@ -33,7 +33,7 @@ class UserLoginForm(StyleFormMixin, AuthenticationForm):
 class UserUpdateForm(UserForm):
     class Meta:
         model = User
-        fields = ('email', 'first_name', 'last_name', 'phone', 'telegram', 'avatar')
+        fields = ('email', 'first_name', 'last_name', 'phone', 'telegram', 'max_messenger' , 'avatar')
 
 
 class UserPasswordResetForm(forms.Form):

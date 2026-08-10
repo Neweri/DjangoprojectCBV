f514abf4fb3f29df4036c6c9470f0389fe6ad72f
gritu <gritus22324@gmail.com>
2026-08-10 20:51:54 +0300

    - Проверка возможности поиска

--- dogs/urls.py
+++ dogs/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from dogs.views import index, BreedListView, DogBreedListView, DogListView, DogCreateView, DogDetailView, DogUpdateView, \
-    DogDeleteView, DogDeactivatedListView, dog_toggle_activity
+    DogDeleteView, DogDeactivatedListView, dog_toggle_activity, DogSearchListView
 from dogs.apps import DogsConfig
 
 
@@ -15,6 +15,7 @@ urlpatterns = [
     #dogs
     path('dogs/', DogListView.as_view(), name='dogs_list'),
     path('dogs/deactivated', DogDeactivatedListView.as_view(), name='dogs_list_deactivated'),
+    path('dogs/search/', DogSearchListView.as_view(), name='dogs_search'),
     path('dogs/create/', DogCreateView.as_view(), name='dog_create'),
     path('dogs/detail/<int:pk>/', DogDetailView.as_view(), name='dog_detail'),
     path('dogs/update/<int:pk>/', DogUpdateView.as_view(), name='dog_update'),
--- dogs/views.py
+++ dogs/views.py
@@ -6,6 +6,7 @@ from django.views.generic import ListView, CreateView, DetailView, UpdateView, D
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.forms import inlineformset_factory
 from django.core.exceptions import PermissionDenied
+from django.db.models import Q
 
 from dogs.models import Breed, Dog, DogParent
 from dogs.forms import DogForm, DogParentForm, DogCreateForm, DogAdminForm
@@ -77,6 +78,16 @@ class DogDeactivatedListView(LoginRequiredMixin, ListView):
         if self.request.user.role == UserRoles.USER:
             queryset = queryset.filter(is_active=False, owner=self.request.user)
         return queryset
+
+class DogSearchListView(ListView):
+    model = Dog
+    template_name = 'dogs/dogs_search_results.html'
+    queryset = Dog.objects.filter(name__icontains='м')
+
+    def get_queryset(self):
+        return Dog.objects.filter(
+            Q(name__icontains='м')
+        )
 
 
 class DogCreateView(LoginRequiredMixin, CreateView):
